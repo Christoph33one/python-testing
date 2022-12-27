39d0a3151ec58ec8fcbccd2653bc85560905fa19
Christoph33one <cjbrowning23@hotmail.com>
2022-12-27 14:42:47 +0000

    importing patch extension and seeing how mocking works when testing

--- test_student.py
+++ test_student.py
@@ -1,11 +1,29 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+    def tearDown(self):
+        print("tear down")
+
     def test_full_name(self):
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
         # assertEqual() in Python is a unittest library function that is
         # used in unit testing to check the equality of two values.
         # This function will take three parameters as input and return
@@ -14,14 +32,38 @@ class TestStudent(unittest.TestCase):
         # will return true else return false.
 
     def test_alert_santa(self):
-        student = Student("John",  "Doe")
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
-        # assertTrue() in Python is a unittest library function
-        #  that is used in unit testing to compare test value with true.
-        #  This function will take two parameters as input and return a
-        # boolean value depending upon the assert condition. If test
-        # value is true then assertTrue() will return true else return false.
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
 
 
 if __name__ == "__main__":
